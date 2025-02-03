84996862a742163b1f370c30548f6db6f98c86f5
red-malone <rohit.psudarsanan@gmail.com>
2025-02-03 20:34:33 +0530

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:todoist/firebase_options.dart';
 import 'package:todoist/screens/home.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,8 +1,18 @@
 class Tasks{
-
+  // String id;
   String title;
   String description;
   bool isDone;
 
   Tasks({required this.title, required this.description, this.isDone=false});
+  Map<String, dynamic> toMap(){
+    return {
+      // 'id': id,
+      'title': title,
+      'description': description,
+      'isDone': isDone,
+    };
+  }
+
+
 }
